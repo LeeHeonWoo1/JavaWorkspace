9cbb3166473c856bb0712d20b048dbbc6e077f55
LeeHeonWoo1 <gjsdn245@naver.com>
2023-05-04 15:02:12 +0900

    Method 정의, 호출

---
+++ src/chap_06/_01_Method.java
@@ -0,0 +1,14 @@
+package chap_06;
+
+public class _01_Method {
+    // 인사문구를 출력하는 메소드 정의
+    public static void sayHello() {
+        System.out.println("Hello? I'm a method.");
+    }
+    public static void main(String[] args) {
+        // 메소드 : 어떠한 기능을 하는 코드들의 묶음
+
+        // 메소드 호출
+        sayHello();
+    }
+}
